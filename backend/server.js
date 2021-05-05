92666d27e3e7407193bca0143c152a1a29897194
Orkun <paulmckiller@users.noreply.github.com>
2021-05-05 23:11:53 +0300

    mongodb connection

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js'
 import products from './data/products.js';
 
 //#region 
@@ -28,6 +29,8 @@ import products from './data/products.js';
 //#endregion
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
