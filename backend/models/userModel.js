3918385b37328c08f65731b0e3680c28ae2de7e9
Orkun <paulmckiller@users.noreply.github.com>
2021-05-06 20:53:57 +0300

    productmodel, usermodel

---
+++ backend/models/userModel.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        isRequired: true
+    },
+    email: {
+        type: String,
+        isRequired: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        isRequired: true
+    },
+    isAdmin: {
+        type: Boolean,
+        isRequired: true,
+        default: false
+    },
+}, { timestamp: true, }
+);
+
+const User = mongoose.Model('User', userSchema);
+
+export default User;
